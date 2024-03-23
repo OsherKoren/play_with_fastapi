c1721d9f83568ae8cff04a049767b68ad419f15e
Osher Koren <oshrats@gmail.com>
2024-03-23 15:38:37 +0200

    Modified create db

--- setup_db/02_create_db.sh
+++ setup_db/02_create_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create the database based on environment variable
-if psql -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw "V"; then
+if psql -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw "$POSTGRES_DB"; then
   echo "Database $POSTGRES_DB already exists"
 else
   psql -U "$POSTGRES_USER" -c "CREATE DATABASE $POSTGRES_DB;"
