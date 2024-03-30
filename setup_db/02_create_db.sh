7e6897acb2ffc02cd5ee8947d062e342a697e3aa
Osher Koren <oshrats@gmail.com>
2024-03-30 09:00:10 +0300

    Modified workflows

--- setup_db/02_create_db.sh
+++ setup_db/02_create_db.sh
@@ -4,6 +4,7 @@
 if psql -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw "$POSTGRES_DB"; then
   echo "Database $POSTGRES_DB already exists"
 else
+  echo "Creating database $POSTGRES_DB"
   psql -U "$POSTGRES_USER" -c "CREATE DATABASE $POSTGRES_DB;"
   echo "Database $POSTGRES_DB created successfully"
 fi
