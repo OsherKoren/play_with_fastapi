3f598eafa45d20d9949cbe5be51e249bb7067eeb
Osher Koren <oshrats@gmail.com>
2024-04-23 12:49:24 +0300

    Added back env file

--- worker/src/kafka/constants.py
+++ worker/src/kafka/constants.py
@@ -3,6 +3,11 @@
 """Kafka constants."""
 import os
 
+from dotenv import load_dotenv
+
+ENV_FILE = ".env.dev" if os.getenv("DEV_ENV") else ".env"
+load_dotenv(ENV_FILE)
+
 KAFKA_HOST = os.getenv("KAFKA_HOST", "kafka")
 KAFKA_PORT = os.getenv("KAFKA_PORT", "9092")
 BOOTSTRAP_SERVERS: str = f"{KAFKA_HOST}:{KAFKA_PORT}"
